134c37fea875fa3bc223c5e471fe16e544572702
C F Kwok <kcfkwok@gmail.com>
2018-07-27 17:21:34 +0800

    add course examples

---
+++ simp_py_examples/course/SM002/t203.py
@@ -0,0 +1,10 @@
+from simp_py import tft
+lcd = tft.tft
+def hello(your_name):
+  global lcd
+  lcd.clear()
+  lcd.text(0,0,'hello')
+  lcd.text(0,20,'your name is %s' % your_name)
+
+if __name__=='__main__':
+  hello('CF')
