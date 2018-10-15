b56e95ba297ffa9736ebaa1de01d723208679708
C F Kwok <kcfkwok@gmail.com>
2018-10-15 17:11:22 +0800

    update S1806

---
+++ simp_py_examples/course/S1806_4/t007_cnt.py
@@ -0,0 +1,9 @@
+# t007.py
+from simp_py import lcd
+import time
+cnt=10
+while cnt >=0:
+    lcd.text(10,10,"count: %s" % cnt)
+    cnt -=1
+    time.sleep(1)
+    
---
+++ simp_py_examples/course/S1806_4/t011_inp.py
@@ -0,0 +1,11 @@
+# t011.py
+from machine import Pin
+from simp_py import lcd
+import time
+inp= Pin(21, Pin.IN)
+while True:
+  if inp.value()==0:
+    lcd.text(0,100,” on”)
+  else:
+    lcd.text(0,100,”off”)
+  time.sleep(0.1)
